0967d4686d2fab7db626a7ec459b2dc0ac4d2c94
Víctor <victorgabrielcruzpereira21@gmail.com>
2024-03-20 19:30:44 -0300

    .

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,20 @@
 function myFunction() {
-    var x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
   }
+}
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    document.querySelectorAll('.fade-in').forEach(function (item) {
+      item.classList.add('fade-in');
+    });
+  }, 500);
+});
+window.addEventListener('load', function() {
+  setTimeout(function() {
+      document.querySelector('.widget').classList.remove('animate');
+  }, 1000);
+});
